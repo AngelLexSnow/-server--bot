29f15d495f54aa8a1e704fe9da3928f87896bedb
AngelLexSnow <alexislucas9758@gmail.com>
2018-02-26 19:43:29 +0100

    Update kernel.js

--- kernel.js
+++ kernel.js
@@ -29,7 +29,7 @@ kernel.on ('ready',() => {
         kernel.user.setPresence({game:{name: 'Ferder - Album',type: 2}});
         break;
     case (modif_horaie_server_heure >= 18):
-         kernel.user.setStatus('online')
+         kernel.user.setStatus('dnd')
          kernel.user.setPresence({game:{name: 'La voie du destin',type: 0}});
         break;
     case (modif_horaie_server_heure >= 23):
